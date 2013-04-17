4be290a39467809257861a1130a39b9ce7ac4563
Raphael Dümig <duemig@in.tum.de>
2013-04-17 09:55:54 +0200

    improved testcase for eigenvalue computation in fwave-solver.c
    now 2 pairs of eigenvalues will be tested

--- src/FWaveSolverTest.hxx
+++ src/FWaveSolverTest.hxx
@@ -14,14 +14,26 @@ public:
         qr.h  = 4.0;
         qr.hu = 4.0;
         
-        lambda1 = 1 - 4.95227220577;
-        lambda2 = 1 + 4.95227220577;
+        lambda1 = -3.95227220577;
+        lambda2 = 5.95227220577;
         
         Vector2D test_lambda = roe_eigenvals(ql, qr);
         
         TS_ASSERT_DELTA(lambda1, test_lambda.x, 1.0E-9);
         TS_ASSERT_DELTA(lambda2, test_lambda.y, 1.0E-9);
         
+        ql.h  = 1.3;
+        ql.hu = 0.6;
+        qr.h  = 2.9;
+        qr.hu = 4.1;
+        
+        lambda1 = -3.50692249113;
+        lambda2 = 5.57074240685;
+        
+        test_lambda = roe_eigenvals(ql, qr);
+        
+        TS_ASSERT_DELTA(lambda1, test_lambda.x, 1.0E-9);
+        TS_ASSERT_DELTA(lambda2, test_lambda.y, 1.0E-9);
     }
     
     // void testSteadyState();
