0ceb5d754f6828bf9fdbb98b3fffdb0d4dcdffed
Raphael Dümig <duemig@in.tum.de>
2013-04-27 12:49:40 +0200

    fixed testing of private functions
    1.) added #define private public in FWaveSolverTest.hpp
    2.) easy changing from float to double: defined TYPE

--- src/FWaveSolverTest.hpp
+++ src/FWaveSolverTest.hpp
@@ -1,21 +1,25 @@
 #include <cxxtest/TestSuite.h>
+#define private public
+
 #include "FWave.hpp"
 
-#define PRIVATE_PUBLIC
+#define TYPE double
+#define MAXERROR 1.0E-9
+
 
 using namespace solver;
 
 class FWaveSolverTest : public CxxTest::TestSuite
 {
 public:
-    /* void xtestEigenvalues() {
-        FWave<float> solver;
+    void testEigenvalues() {
+        FWave<TYPE> solver;
         
-        float hl, hr;
-        float hul, hur;
+        TYPE hl, hr;
+        TYPE hul, hur;
         
-        float lambda[2];
-        float test_lambda[2];
+        TYPE lambda[2];
+        TYPE test_lambda[2];
         
         hl  = 1.0;
         hul = 1.0;
@@ -27,8 +31,8 @@ public:
         
         solver.roeEigenvals(hl, hr, hul, hur, test_lambda[0], test_lambda[1]);
         
-        TS_ASSERT_DELTA(lambda[0], test_lambda[0], 1.0E-9);
-        TS_ASSERT_DELTA(lambda[1], test_lambda[1], 1.0E-9);
+        TS_ASSERT_DELTA(lambda[0], test_lambda[0], MAXERROR);
+        TS_ASSERT_DELTA(lambda[1], test_lambda[1], MAXERROR);
         
         
         hl  = 1.3;
@@ -41,33 +45,33 @@ public:
         
         solver.roeEigenvals(hl, hr, hul, hur, test_lambda[0], test_lambda[1]);
         
-        TS_ASSERT_DELTA(lambda[0], test_lambda[0], 1.0E-9);
-        TS_ASSERT_DELTA(lambda[1], test_lambda[1], 1.0E-9);
+        TS_ASSERT_DELTA(lambda[0], test_lambda[0], MAXERROR);
+        TS_ASSERT_DELTA(lambda[1], test_lambda[1], MAXERROR);
     }
     
-    void xtestFlux() {
-        FWave<float> solver;
+    void testFlux() {
+        FWave<TYPE> solver;
         
-        float h  = 4.3;
-        float hu = 2.5;
+        TYPE h  = 4.3;
+        TYPE hu = 2.5;
         
-        float fl[2];
+        TYPE fl[2];
         solver.flux(h, hu, fl[0], fl[1]);
         
-        TS_ASSERT_DELTA(fl[0],      2.5, 1.0E-9);
-        TS_ASSERT_DELTA(fl[1], 96.94345, 1.0E-9);
+        TS_ASSERT_DELTA(fl[0],      2.5, MAXERROR);
+        TS_ASSERT_DELTA(fl[1], 96.94345, MAXERROR);
     }
     
-    void xtestEigencoefficients() {
-        FWave<float> solver;
+    void testEigencoefficients() {
+        FWave<TYPE> solver;
         
-        float hul = 1.5;
-	    float hur = 3.0;
-	    float hl  = 1.0;
-	    float hr  = 0.73572032979;
+        TYPE hul = 1.5;
+	    TYPE hur = 3.0;
+	    TYPE hl  = 1.0;
+	    TYPE hr  = 0.73572032979;
         
-	    float eigenvals[2];
-        float ec[2];
+	    TYPE eigenvals[2];
+        TYPE ec[2];
         
         eigenvals[0] = -0.9;
         eigenvals[1] =  1.4;
@@ -76,17 +80,17 @@ public:
         
         solver.eigencoeffis(hl, hr, hul, hur, eigenvals[0], eigenvals[1], ec[0], ec[1]);
         
-        TS_ASSERT_DELTA(ec[0], -1.04347826087, 1.0E-9);
-        TS_ASSERT_DELTA(ec[1],  2.54347826087, 1.0E-9);
+        TS_ASSERT_DELTA(ec[0], -1.04347826087, MAXERROR);
+        TS_ASSERT_DELTA(ec[1],  2.54347826087, MAXERROR);
     }
-    */
+    
     void testSteadyState() {
-        FWave<float> solver;
+        FWave<TYPE> solver;
         
-        float hl, hr, hul, hur;
+        TYPE hl, hr, hul, hur;
         
-        float updateLeft[2], updateRight[2];
-        float maxSpeed;
+        TYPE updateLeft[2], updateRight[2];
+        TYPE maxSpeed;
         
         hl  = hr  = 2.4;
         hul = hur = 3.7;
@@ -97,10 +101,10 @@ public:
                 updateLeft[1], updateRight[1],
                 maxSpeed );
         
-        TS_ASSERT_DELTA(updateLeft[0], 0.0, 1.0E-9);
-        TS_ASSERT_DELTA(updateLeft[1], 0.0, 1.0E-9);
-        TS_ASSERT_DELTA(updateRight[0], 0.0, 1.0E-9);
-        TS_ASSERT_DELTA(updateRight[1], 0.0, 1.0E-9);
+        TS_ASSERT_DELTA(updateLeft[0], 0.0, MAXERROR);
+        TS_ASSERT_DELTA(updateLeft[1], 0.0, MAXERROR);
+        TS_ASSERT_DELTA(updateRight[0], 0.0, MAXERROR);
+        TS_ASSERT_DELTA(updateRight[1], 0.0, MAXERROR);
         
     }
     
